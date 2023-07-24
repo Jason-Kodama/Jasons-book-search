882634204f0fc1b382cbc7e6a423f5d0e56c03d5
Jason <jasonkodama@gmail.com>
2023-07-24 15:00:29 -0600

    added code to server folder

---
+++ server/config/connection.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/book-collection', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  dbName: 'book-search-db',
+});
+
+module.exports = mongoose.connection;
