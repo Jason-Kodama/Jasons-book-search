204e7cee06ee1bc80e33576b5ec6e94fb57c655a
Jason <jasonkodama@gmail.com>
2023-07-25 15:43:57 -0600

    changes to server and pages

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,11 +1,10 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/book-collection', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/googlebooks', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
   useFindAndModify: false,
-  dbName: 'book-search-db',
 });
 
 module.exports = mongoose.connection;
--- server/server.js
+++ server/server.js
@@ -8,11 +8,20 @@ const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  context: authMiddleware,
-})
+
+const startServer = async () => {
+
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: authMiddleware,
+  });
+  await server.start();
+  server.applyMiddleware({ app });
+  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`)
+};
+
+startServer();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -22,23 +31,6 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+db.once('open', () => {
+    app.listen(PORT, () => console.log(`Now listening on localhost:${PORT}`));
 });
-
-const startApolloServer = async (typeDefs, resolvers) => {
-  await server.start();
-  server.applyMiddleware({ app });
-
-  db.once('open', () => {
-    app.listen(PORT, () => {
-      console.log(`API server running on port ${PORT}!`);
-      console.log(`GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
-    })
-  })
-};
-
-startApolloServer(typeDefs, resolvers);
-
-
-
